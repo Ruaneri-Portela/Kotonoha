a319bf31add68880253b977099c994f1057cb996
Ruaneri Portela <Ruaneri Portela>
2023-10-23 20:03:19 -0300

    Add android compiler configs, on Visual Studio to AMD64/ARM8/ARM7neon

---
+++ Android/app/src/main/java/com/Kotonoha_Android/Kotonoha_Android.java
@@ -0,0 +1,16 @@
+
+package com.Kotonoha_Android;
+
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class Kotonoha_Android extends SDLActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
+
